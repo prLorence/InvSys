b364b25778b802cc2d0bddb5cd323f7663fa7217
prLorence <paulorencereyes@gmail.com>
2023-08-03 12:54:30 +0800

    feat(common): validation problem signature overload

--- src/InvSys.Application/Common/ApiControllerBase.cs
+++ src/InvSys.Application/Common/ApiControllerBase.cs
@@ -1,6 +1,7 @@
 using MediatR;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace InvSys.Application.Common;
@@ -11,4 +12,37 @@ public class ApiControllerBase : ControllerBase
     private ISender? _mediator;
 
     protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetService<ISender>()!;
+
+    protected ActionResult Problem(IEnumerable<IDictionary<string, object>> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+
+        var errorMap = errors
+          .SelectMany(dictionary => dictionary)
+          .Select(item => new KeyValuePair<string, object>(item.Key, item.Value));
+
+        foreach (KeyValuePair<string, object> error in errorMap)
+        {
+            modelStateDictionary.AddModelError(error.Key, error.Value.ToString() ?? "False Error (help me)");
+        }
+
+        return ValidationProblem(modelStateDictionary);
+    }
+
+    protected ActionResult Problem(IEnumerable<string> messages)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+
+        // var errorMap = messages
+        //   .SelectMany(dictionary => dictionary)
+        //   .Select(item => new KeyValuePair<string, object>(item.Key, item.Value));
+
+        foreach (var message in messages)
+        {
+            modelStateDictionary.AddModelError(message, message ?? "False Error (help me)");
+        }
+
+        return ValidationProblem(modelStateDictionary);
+    }
+
 }
