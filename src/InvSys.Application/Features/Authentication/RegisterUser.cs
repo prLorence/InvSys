c38b66641d970232a87d4eab4abb713dd5ec7112
prLorence <paulorencereyes@gmail.com>
2023-08-03 12:51:34 +0800

    fix(features/register): better errors

--- src/InvSys.Application/Features/Authentication/RegisterUser.cs
+++ src/InvSys.Application/Features/Authentication/RegisterUser.cs
@@ -1,5 +1,15 @@
-using InvSys.Application.Common;
+using System.Reflection;
+using System.Security.Claims;
 
+using FluentResults;
+
+using InvSys.Application.Common;
+using InvSys.Application.Common.Interfaces;
+using InvSys.Application.Entities;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InvSys.Application.Features.Authentication;
@@ -8,12 +18,80 @@ namespace InvSys.Application.Features.Authentication;
 public class RegisterUserController : ApiControllerBase
 {
     [HttpPost]
-    public async Task<ActionResult> Register()
+    public async Task<ActionResult> Register(RegisterUserCommand command)
     {
         // return await Mediator.Send();
-        await Task.CompletedTask;
+        var registerResult = await Mediator.Send(command);
 
-        return Ok();
+        if (registerResult.IsFailed)
+        {
+            // this line is fugly
+            // return Problem(registerResult.Errors.Select(
+            //             e => new Dictionary<string, object>()
+            //             {
+            //                         { e., e.Message }
+            // }));
+            return Problem(registerResult.Reasons.Select(
+                        r => new Dictionary<string, object>()
+                        {
+                            { "Details", r.Message }
+                        }
+            ));
+            // var errors = registerResult.Errors.Select(e => e.Metadata);
+            // return Problem(errors);
+        }
+
+        return Ok(registerResult.Value);
     }
 }
 
+public record RegisterUserResult(
+        ApplicationUser User,
+        string Token);
+
+public record RegisterUserCommand(
+        string Username,
+        string Email,
+        string Password) : IRequest<Result<RegisterUserResult>>;
+
+public class RegisterUser : IRequestHandler<RegisterUserCommand, Result<RegisterUserResult>>
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public RegisterUser(
+        UserManager<ApplicationUser> userManager,
+        IJwtTokenGenerator jwtTokenGenerator,
+        SignInManager<ApplicationUser> signInManager)
+    {
+        _userManager = userManager;
+        _jwtTokenGenerator = jwtTokenGenerator;
+        _signInManager = signInManager;
+    }
+
+    public async Task<Result<RegisterUserResult>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+    {
+        var newUser = new ApplicationUser
+        {
+            Id = Guid.NewGuid(),
+            Email = request.Email,
+            UserName = request.Username,
+            NormalizedEmail = request.Email.ToUpper(),
+            NormalizedUserName = request.Username.ToUpper()
+        };
+
+        var identityUser = await _userManager.CreateAsync(newUser, request.Password);
+
+        if (!identityUser.Succeeded)
+        {
+            return Result.Fail<RegisterUserResult>(identityUser.Errors.Select(e => e.Description));
+        }
+
+        var jwt = _jwtTokenGenerator.Generate(newUser);
+
+        return new RegisterUserResult(
+                newUser,
+                jwt);
+    }
+}
